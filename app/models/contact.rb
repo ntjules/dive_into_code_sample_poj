77f4e353e8410dc71fe7a28af30a5b46fa4774e3
jules <ntjules@gmail.com>
2018-03-14 09:23:04 +0000

    adding validation to email

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,5 @@ class Contact < ApplicationRecord
      validates :content,    length: { in: 1..140 } 
       validates :name, presence: true
        validates :email, presence: true
+       validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
